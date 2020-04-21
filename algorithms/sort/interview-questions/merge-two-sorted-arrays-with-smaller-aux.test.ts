d2ef5f43b9f7e8b530ac2140b7883b71c7c9d709
Linh Truong <thlinh8392@outlook.com>
2020-04-21 14:36:39 +0700

    feat(merge-smaller-aux): add solution for review questions

---
+++ algorithms/sort/interview-questions/merge-two-sorted-arrays-with-smaller-aux.test.ts
@@ -0,0 +1,56 @@
+/*
+Merging with smaller auxiliary array. Suppose that the subarray 𝚊[𝟶] to 𝚊[𝚗−𝟷] is sorted and the subarray 𝚊[𝚗] to 𝚊[𝟸∗𝚗−𝟷] is sorted.
+ How can you merge the two subarrays so that 𝚊[𝟶] to 𝚊[𝟸∗𝚗−𝟷] is sorted using an auxiliary array of length n?
+*/
+
+import { getRandomInt, isCorrectOrder } from '../../../util/helper.util';
+
+describe('Merge two sorted arrays with smaller auxiliary array', () => {
+  it('test', () => {
+    const arr = generateArr(1000);
+    const comparator = (n1: number, n2: number): -1 | 0 | 1 => {
+      if (n1 === n2) return 0;
+      return n1 < n2 ? -1 : 1;
+    };
+
+    sort(arr, 0, arr.length - 1, comparator);
+
+    expect(isCorrectOrder(arr, comparator));
+  });
+
+    function generateArr(n: number): number[] {
+      const arr = new Array(n);
+      for (let i = 0; i < n; i++) {
+        arr[i] = getRandomInt(n);
+      }
+      return arr;
+    }
+});
+
+function merge<T>(arr: T[], lo: number, mid: number, hi: number, comparator: (t1: T, t2: T) => -1 | 0 | 1): void {
+  const auxLength = hi - lo + 1;
+  const aux: T[] = new Array<T>(auxLength);
+
+  for (let k = lo; k <= mid; k++) {
+    aux[k] = arr[k];
+  }
+
+  let i = lo;
+  let j = mid + 1;
+
+  for (let k = lo; k <= hi; k++) {
+    if (i > mid) arr[k] = arr[j++];
+    else if (j > hi) arr[k] = aux[i++];
+    else if (comparator(aux[i], arr[j]) <= 0) arr[k] = aux[i++];
+    else arr[k] = arr[j++];
+  }
+  return;
+}
+
+function sort<T>(arr: T[], lo: number, hi: number, comparator: (t1: T, t2: T) => -1 | 0 | 1): void {
+  if (lo >= hi) return;
+  const mid = lo + Math.floor((hi - lo) / 2);
+  sort(arr, lo, mid, comparator);
+  sort(arr, mid + 1, hi, comparator);
+  merge(arr, lo, mid, hi, comparator);
+}
